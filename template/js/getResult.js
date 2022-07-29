27cc23774c084a884047d4a83caf9b19c399b4b7
LekiaAnonim <lekiaprosper@gmail.com>
2022-07-29 13:40:36 +0100

    change the background color of cancel button on hover

--- template/js/getResult.js
+++ template/js/getResult.js
@@ -61,15 +61,15 @@ function renderResult(e) {
                     resultHTML += `<div style="width: 400px; max-height:400px; overflow-y: scroll; padding: 0 10px; position:absolute; display:block; background-color:#fff; border: 1px solid #c8d9e7;">`;
                     resultHTML += `<div class="resulthead" style="position: fixed; background-color:#fff; margin-top: 0; padding-top:5px;">
                                         <div style="width: 380px; margin-top:20px; padding-left:5px; background-color:#7CB9E8; color:#fff;">${searchText}</div>
-                                        <div class="cancel" style="cursor:pointer; display:flex; align-items:center; justify-content:center; text-justify:center; width:15px; height:15px; position:absolute; top:0; right:0; color:#fff; background-color: rgb(68, 66, 66); border-radius:50%; font-size:8px;">✖</div>
+                                        <div class="cancel" style="cursor:pointer; display:flex; align-items:center; justify-content:center; text-justify:center; width:15px; height:15px; position:absolute; top:0; right:0; color:#fff; border-radius:50%; font-size:8px;">✖</div>
                                         <div style="display:flex; align-items:center; justify-content: space-between;">
                                           <div class="navigatepage" style="">
-                                            <button class="previous pagenav" style="display: none; border:none; background-color:none; font-size:x-small;"><< Previous</button>
+                                            <button class="previous pagenav" style="display: none; border:none; background-color:#fff; font-size:x-small;"><< Previous</button>
                                             <p style="display: none; background-color:rgb(147, 201, 252);font-size:x-small;">${offset+1} of ${data.total}</p>
-                                            <button class="next pagenav" style="display: none; border:none; background-color:none;font-size:x-small;">Next >></button>
+                                            <button class="next pagenav" style="display: none; border:none; background-color:#fff;font-size:x-small;">Next >></button>
                                           </div>
                                           <div class="tooltip">
-                                            <button class="copybtn" style="border:none; background-color:none; font-size:x-small;" onclick="copyResult()" onmouseout="outFunc()">
+                                            <button class="copybtn" style="border:none; background-color:#fff; font-size:x-small;" onclick="copyResult()" onmouseout="outFunc()">
                                             <span class="tooltiptext" id="myTooltip">Copy to clipboard</span>
                                             <i style="font-size:15px; margin-top:5px;" class="fa">&#xf24d;</i>
                                             </button>
@@ -101,7 +101,7 @@ function renderResult(e) {
                     resultHTML += `<div style="width: 400px; height:400px; overflow-y: scroll; padding: 0 10px; position:absolute; display:block; background-color:#fff; border: 1px solid #c8d9e7;">`;
                     resultHTML += `<div class="resulthead" style="position: fixed; background-color:#fff; margin-top: 0; padding-top:5px;">
                               <div class="searchtext" style="width: 380px; margin-top:20px; padding-left:5px; background-color:#7CB9E8; color:#fff;">${searchText}</div>
-                              <div class="cancel" style="cursor:pointer; display:flex; align-items:center; justify-content:center; text-justify:center; width:15px; height:15px; position:absolute; top:0; right:0; color:#fff; background-color: rgb(68, 66, 66); border-radius:50%; font-size:8px;">✖</div>
+                              <div class="cancel" style="cursor:pointer; display:flex; align-items:center; justify-content:center; text-justify:center; width:15px; height:15px; position:absolute; top:0; right:0; color:#fff; border-radius:50%; font-size:8px;">✖</div>
                               <div style="display:flex; align-items:center; justify-content: space-between;">
                                 <div class="navigatepage" style="display: flex;">
                                   <button class="previous pagenav" style="border:none; background-color:none; font-size:x-small;"><< Previous</button>
